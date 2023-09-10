53735a48895a47e8822c526de9de498d994a181f
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-10 12:17:32 +0500

    8.1.4 Придумайте простой класс, который будет предоставлять информацию об установленном в системе диске.  Нужны свойства, чтобы хранить: имя диска , объём, свободное место. Свойства инициализируются при создании нового объекта в методе-конструкторе.

---
+++ FileSystemEdu/FileSystemEdu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary>
+/// Придумайте простой класс, который будет предоставлять информацию об установленном в системе диске. 
+/// Нужны свойства, чтобы хранить: имя диска, объём, свободное место. 
+/// Свойства инициализируются при создании нового объекта в методе-конструкторе.
+/// </summary>
+namespace FileSystemEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Disk
+    {
+        public string Name { get; }
+        public double TotalSize { get; }
+        public double FreeSize { get; private set; }
+        public Disk(string diskName, double diskSize, double diskFreeSize)
+        {
+            Name = diskName;
+            TotalSize = diskSize;
+            FreeSize = diskFreeSize;
+        }
+    }
+
+}
