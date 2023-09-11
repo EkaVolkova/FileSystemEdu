9f5fc63b26639a1a4726457d10b08aedc7a4976f
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-11 22:11:56 +0500

    8.4.3 Дан класс:
    public string Name { get; set; }
    public long PhoneNumber { get; set; }
    public string Email { get; set; }
    
    public Contact(string name, long phoneNumber, string email)
    {
       Name = name;
       PhoneNumber = phoneNumber;
       Email = email;
    }
    Доработайте его и сериализуйте в бинарный формат.

---
+++ FileSystemEdu/Serialization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/// <summary>
+/// 8.4.3. Дан класс Contact. Доработайте его и сериализуйте в бинарный формат
+/// </summary>
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contact = new Contact("Tom", 9876534567, "tom@gmail.com");
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (var fs = new FileStream("mycontact.bin", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, contact);
+            }
+        }
+    }
+    [Serializable]
+    class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
