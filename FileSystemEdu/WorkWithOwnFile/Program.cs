5e6e2f3a8fae5020251452188ca9a5bf09c53a47
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-11 21:23:07 +0500

    8.3. Напишите программу, которая выводит свой собственный исходный код в консоль.

---
+++ FileSystemEdu/WorkWithOwnFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+/// <summary>
+/// Напишите программу, которая выводит свой собственный исходный код в консоль.
+/// </summary>
+namespace WorkWithOwnFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\projects\skillfactory\FileSystemEdu\FileSystemEdu\WorkWithOwnFile\Program.cs";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string str = String.Empty;
+                // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+                while ((str = sr.ReadLine()) != null) 
+                {
+                    Console.WriteLine(str);
+                }
+
+            }
+        }
+    }
+}
